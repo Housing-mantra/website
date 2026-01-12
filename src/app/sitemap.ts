413117ca22b93ac55bf44d939fd70696c03f1fd3
Rajnish Kumar <rajnishkumar@MacBookPro.alphionsee.in>
2026-01-12 19:33:28 +0530

    Fix static export configuration: remove invalid config and set static routes

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -2,6 +2,8 @@
 import { MetadataRoute } from 'next';
 import { PROJECTS } from '@/lib/data';
 
+export const dynamic = "force-static";
+
 export default function sitemap(): MetadataRoute.Sitemap {
     const baseUrl = 'https://housingmantra.in';
 
